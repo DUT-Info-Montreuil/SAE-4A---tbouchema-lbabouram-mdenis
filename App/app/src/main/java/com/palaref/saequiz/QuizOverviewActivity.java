8bbe61155a4c9ea32260fff112e0ded2dc2f4462
Tarook <Tarook.Zinou@gmail.com>
2023-04-03 17:42:10 +0200

    Started creating the activity

---
+++ App/app/src/main/java/com/palaref/saequiz/QuizOverviewActivity.java
@@ -0,0 +1,14 @@
+package com.palaref.saequiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_overview);
+    }
+}
