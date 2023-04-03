89b308a243433682790775670e7523cdc5c8c538
Tarook <Tarook.Zinou@gmail.com>
2023-04-03 22:12:51 +0200

    started working on game implementation. Have to make the table in the db

--- App/app/src/main/java/com/palaref/saequiz/QuizGameActivity.java
+++ App/app/src/main/java/com/palaref/saequiz/QuizGameActivity.java
@@ -2,13 +2,27 @@ package com.palaref.saequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.palaref.saequiz.databinding.ActivityQuizGameBinding;
+import com.palaref.saequiz.model.QuizGame;
+import com.palaref.saequiz.utils.AnswerAdapter;
+
 public class QuizGameActivity extends AppCompatActivity {
+
+    private ActivityQuizGameBinding binding;
+    private QuizGame quizGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityQuizGameBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_quiz_game);
+
+        Intent intent = getIntent();
+        int quizId = intent.getIntExtra("quizId", -1);
+
+        // get quiz from database
     }
 }
