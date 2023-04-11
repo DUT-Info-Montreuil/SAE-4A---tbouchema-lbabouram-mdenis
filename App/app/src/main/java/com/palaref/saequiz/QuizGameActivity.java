e814ae1dd699349f4b95d5566e81b5d2c123212a
Tarook <Tarook.Zinou@gmail.com>
2023-04-11 22:48:27 +0200

    Problem with answers list or iscorrect check being weird

--- App/app/src/main/java/com/palaref/saequiz/QuizGameActivity.java
+++ App/app/src/main/java/com/palaref/saequiz/QuizGameActivity.java
@@ -16,7 +16,7 @@ import com.palaref.saequiz.utils.SQLiteManager;
 
 public class QuizGameActivity extends AppCompatActivity {
 
-    private ActivityQuizGameBinding binding;
+    //private ActivityQuizGameBinding binding;
     private QuizGame quizGame;
     private TextView questionTextView, scoreTextView, multiplierTextView;
     private Button answer1Button, answer2Button, answer3Button, answer4Button;
@@ -24,7 +24,7 @@ public class QuizGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityQuizGameBinding.inflate(getLayoutInflater());
+        //binding = ActivityQuizGameBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_quiz_game);
 
         Intent intent = getIntent();
@@ -34,18 +34,18 @@ public class QuizGameActivity extends AppCompatActivity {
         quizGame = sqLiteManager.getQuizGameByQuizInfoId(quizId);
 
         setupViews();
-        nextQuestion();
         setupListeners();
+        nextQuestion();
     }
 
     private void setupViews() {
-        questionTextView = binding.questionTextviewQuizGame;
-        scoreTextView = binding.scoreTextviewQuizGame;
-        multiplierTextView = binding.scoremultiplierTextviewQuizGame;
-        answer1Button = binding.answer1ButtonGame;
-        answer2Button = binding.answer2ButtonGame;
-        answer3Button = binding.answer3ButtonGame;
-        answer4Button = binding.answer4ButtonGame;
+        questionTextView = findViewById(R.id.question_textview_quiz_game);
+        scoreTextView = findViewById(R.id.score_textview_quiz_game);
+        multiplierTextView = findViewById(R.id.scoremultiplier_textview_quiz_game);
+        answer1Button = findViewById(R.id.answer1_button_game);
+        answer2Button = findViewById(R.id.answer2_button_game);
+        answer3Button = findViewById(R.id.answer3_button_game);
+        answer4Button = findViewById(R.id.answer4_button_game);
     }
 
     private void setupListeners() {
