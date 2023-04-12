c1b025c337780121de1e92c9c038d9443de94913
lbabouram <lionel.joel@sfr.fr>
2023-04-12 21:44:34 +0200

    visuel profil avec login et log out

---
+++ App/app/src/main/java/com/palaref/saequiz/ui/profile/CircularImageView.java
@@ -0,0 +1,60 @@
+package com.palaref.saequiz.ui.profile;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class CircularImageView extends androidx.appcompat.widget.AppCompatImageView {
+
+    public CircularImageView(Context context) {
+        super(context);
+    }
+
+    public CircularImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircularImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Drawable drawable = getDrawable();
+
+        if (drawable == null) {
+            return;
+        }
+
+        if (getWidth() == 0 || getHeight() == 0) {
+            return;
+        }
+
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        Bitmap circularBitmap = getCircularBitmap(bitmap);
+        canvas.drawBitmap(circularBitmap, 0, 0, null);
+    }
+
+    private Bitmap getCircularBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        Paint paint = new Paint();
+        Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawOval(new RectF(rect), paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+}
+
